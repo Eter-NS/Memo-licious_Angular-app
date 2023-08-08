08d32ecccb7bed60968ec957a09dfaa99d4b1a03
Norbert <norbert.sosnowski.front@gmail.com>
2023-08-08 18:03:06 +0200

    Recreating the login-or-guest-view component
    - Removed the emojis
    - Removed router-outlet
    - Added MatRipple for path buttons
    - Finished path buttons layout & styles
    - Added shopping-three SVG template and its responsiveness
    - Added styles for SVG
    - Simplified component style structure

--- src/app/getting-started/login-or-guest-view/login-or-guest-view.component.ts
+++ src/app/getting-started/login-or-guest-view/login-or-guest-view.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-or-guest-view',
   templateUrl: './login-or-guest-view.component.html',
   styleUrls: ['./login-or-guest-view.component.scss'],
 })
-export class LoginOrGuestViewComponent {
-  showRegisterForm() {}
-  showLogInForm() {}
+export class LoginOrGuestViewComponent implements OnInit {
+  svgSize!: string;
+  rippleColor = '';
+
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.changeSVGSize(); // Setting the SVG size after component initial
+  }
+
+  @HostListener('window:resize')
+  private changeSVGSize() {
+    const svgPercentSizeInViewport = 0.6,
+      minimumSize = 225;
+    let chosenValue: number;
+
+    window.innerHeight >= window.innerWidth
+      ? (chosenValue = window.innerWidth)
+      : (chosenValue = window.innerHeight);
+
+    const value = chosenValue * svgPercentSizeInViewport;
+
+    this.svgSize =
+      value >= minimumSize ? value.toString() : minimumSize.toString();
+  }
+
+  goGuestPath() {}
+  goOnlinePath() {}
 }
