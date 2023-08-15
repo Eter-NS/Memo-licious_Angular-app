8837a98ff8b173c413dbda1584ba0d79ce735c09
Norbert <norbert.sosnowski.front@gmail.com>
2023-08-15 18:06:01 +0200

    Recreated svg logic inside svg-elements directive

--- src/reusable/SVGs/svg-element.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SvgElement } from './svg-element';
-
-describe('SvgElement', () => {
-  it('should create an instance', () => {
-    expect(new SvgElement()).toBeTruthy();
-  });
-});
---
+++ src/reusable/SVGs/svg-elements.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SvgElementsDirective } from './svg-elements.directive';
+
+describe('SvgElementsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SvgElementsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
