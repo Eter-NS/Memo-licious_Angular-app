8325cb51d4d1dae59331efb621fc56f9ad660df1
Norbert <norbert.sosnowski.front@gmail.com>
2023-08-15 18:27:39 +0200

    Added angular/fire sdk and changed style paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { GettingStartedModule } from './getting-started/getting-started.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +25,10 @@ import { GettingStartedModule } from './getting-started/getting-started.module';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
   ],
   providers: [],
   bootstrap: [AppComponent],
