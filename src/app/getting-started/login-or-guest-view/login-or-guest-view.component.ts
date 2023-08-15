f41eb782e2d560706b965df42236e133ce4f9fe5
Norbert <norbert.sosnowski.front@gmail.com>
2023-08-15 18:18:53 +0200

    Ripple provided changes to login/guest component
    Added global animations and template refactor thanks to custom ripple directive.
    Added logic for transitions to register and login routes.
    
    Things to change:
    *Guest button must redirect to guest path, not register path, similar thing for login

--- src/app/getting-started/login-or-guest-view/login-or-guest-view.component.ts
+++ src/app/getting-started/login-or-guest-view/login-or-guest-view.component.ts
@@ -1,5 +1,17 @@
-import { Component, HostListener, OnInit, inject } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  addAnimations,
+  removeAnimations,
+} from 'src/app/animations/animation-tools';
+import { runWithDelay } from 'src/app/animations/animation-triggers';
 
 @Component({
   selector: 'app-login-or-guest-view',
@@ -7,6 +19,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-or-guest-view.component.scss'],
 })
 export class LoginOrGuestViewComponent implements OnInit {
+  @ViewChild('container') hostElement!: ElementRef<HTMLElement>;
   svgSize!: string;
   rippleColor = '';
 
@@ -19,9 +32,11 @@ export class LoginOrGuestViewComponent implements OnInit {
   @HostListener('window:resize')
   private changeSVGSize() {
     const svgPercentSizeInViewport = 0.6,
-      minimumSize = 225;
+      minimumSize = 225,
+      maximumSize = 400;
     let chosenValue: number;
 
+    // Takes smaller viewport size as the size of the SVG
     window.innerHeight >= window.innerWidth
       ? (chosenValue = window.innerWidth)
       : (chosenValue = window.innerHeight);
@@ -29,9 +44,30 @@ export class LoginOrGuestViewComponent implements OnInit {
     const value = chosenValue * svgPercentSizeInViewport;
 
     this.svgSize =
-      value >= minimumSize ? value.toString() : minimumSize.toString();
+      value < minimumSize
+        ? minimumSize.toString()
+        : value > maximumSize
+        ? maximumSize.toString()
+        : value.toString();
   }
 
-  goGuestPath() {}
-  goOnlinePath() {}
+  runTransition(suffix: string) {
+    removeAnimations(
+      this.hostElement.nativeElement,
+      'fadeIn-vol-2-animation',
+      true
+    );
+    addAnimations(this.hostElement.nativeElement, 'fade-out-animation', true);
+
+    runWithDelay(this.hostElement.nativeElement.children, {
+      reverse: true,
+      timeoutTime: 1000,
+    })
+      .then(() => {
+        this.router.navigateByUrl(suffix);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 }
