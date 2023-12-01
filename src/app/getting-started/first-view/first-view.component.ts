4562b97d50a52a61439cebc15824fa5ed646ef3d
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2023-12-01 19:35:36 +0100

    Styling changes + additional viewport
    - Added a viewport for devices bigger than mobile

--- src/app/getting-started/first-view/first-view.component.ts
+++ src/app/getting-started/first-view/first-view.component.ts
@@ -1,22 +1,36 @@
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import {
   addAnimations,
   startAnimation,
-} from 'src/app/animations/animation-tools';
-import { runAnimations } from 'src/app/animations/animation-triggers';
+} from 'src/app/reusable/animations/animation-tools';
+import { runAnimations } from 'src/app/reusable/animations/animation-triggers';
+import { LocalStorageService } from 'src/app/reusable/localStorage/local-storage.service';
 
 @Component({
   selector: 'app-first-view',
   templateUrl: './first-view.component.html',
   styleUrls: ['./first-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FirstViewComponent {
-  router = inject(Router);
+export class FirstViewComponent implements OnInit {
+  #router = inject(Router);
+  #localStorageService = inject(LocalStorageService);
 
   @ViewChild('container') sectionContainer!: ElementRef<HTMLDivElement>;
 
-  fillTheViewWithElement() {
+  ngOnInit(): void {
+    this.#localStorageService.saveToStorage('finishedTutorial', false);
+  }
+
+  fillView() {
     addAnimations(
       this.sectionContainer.nativeElement,
       'fill-the-view-animation'
@@ -33,10 +47,10 @@ export class FirstViewComponent {
     );
 
     setTimeout(() => {
-      this.fillTheViewWithElement();
+      this.fillView();
     }, 300);
     setTimeout(() => {
-      this.router.navigateByUrl('/getting-started/choose-path');
+      this.#router.navigateByUrl('/getting-started/choose-path');
     }, 1500);
   }
 }
