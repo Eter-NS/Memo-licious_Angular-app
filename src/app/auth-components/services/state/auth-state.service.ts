aa8aa1600526b918e20d8846036a75e453e3266f
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-01-09 03:22:56 +0100

    Provided tests and secured user Observable

--- src/app/auth-components/services/state/auth-state.service.ts
+++ src/app/auth-components/services/state/auth-state.service.ts
@@ -1,24 +1,24 @@
 import { Injectable, inject, signal } from '@angular/core';
 import {
   Auth,
-  User,
   UserCredential,
   browserLocalPersistence,
   browserSessionPersistence,
   user,
 } from '@angular/fire/auth';
 
-export function isUser(user: User | null): user is User {
-  return user !== null;
-}
-
 @Injectable({
   providedIn: 'root',
 })
 export class AuthStateService {
-  session = signal<UserCredential | null | undefined>(undefined);
   auth = inject(Auth);
-  user$ = user(this.auth);
+  session = signal<UserCredential | null | undefined>(undefined);
+
+  user = user;
+
+  get user$() {
+    return this.user(this.auth);
+  }
 
   constructor() {
     this.auth.setPersistence(browserSessionPersistence);
