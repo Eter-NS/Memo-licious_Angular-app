07d743f447ff58620f665533025a6e02396cb93e
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-02-29 18:02:44 +0100

    Improved routes simplicity in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,22 @@
 import { Routes } from '@angular/router';
-import { redirectLoggedInToGuard } from './auth-components/guards/redirect-logged-in-to.guard';
-import { redirectUnauthorizedToGuard } from './auth-components/guards/redirect-unauthorized-to.guard';
+import { redirectLoggedInToGuard } from './auth/guards/redirect-logged-in-to.guard';
+import { redirectUnauthorizedToGuard } from './auth/guards/redirect-unauthorized-to.guard';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { redirectUnverifiedToGuard } from './auth/guards/online-only/redirect-unverified-to.guard';
 
-export const redirectUnauthorizedToGettingStarted =
-  redirectUnauthorizedToGuard('/getting-started');
+export const redirectUnauthorizedToGettingStartedChoosePath =
+  redirectUnauthorizedToGuard('/getting-started/choose-path');
 export const redirectUnauthorizedToOnline =
   redirectUnauthorizedToGuard('/online');
+export const redirectUnverifiedToVerifyEmail =
+  redirectUnverifiedToGuard('/verify-email');
 export const redirectLoggedInToApp = redirectLoggedInToGuard('/app');
 
 export const appRoutes: Routes = [
   {
     title: 'Create a local account',
     path: 'guest',
-    loadComponent: () =>
-      import('./auth-components/guest/guest.component').then(
-        (m) => m.GuestComponent
-      ),
-  },
-  {
-    title: 'Create an online account',
-    path: 'online',
+    canActivate: [redirectLoggedInToApp],
     children: [
       {
         /*
@@ -29,14 +25,35 @@ export const appRoutes: Routes = [
         */
         path: ':siteAction',
         loadComponent: () =>
-          import('./auth-components/online/online.component').then(
+          import('./auth/guest/guest.component').then((m) => m.GuestComponent),
+      },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./auth/guest/guest.component').then((m) => m.GuestComponent),
+      },
+    ],
+  },
+  {
+    title: 'Create an online account',
+    path: 'online',
+    canActivate: [redirectLoggedInToApp],
+    children: [
+      {
+        /*
+        force=login to force login form before register
+        forward=path to redirect user to specific path after confirmed login
+        */
+        path: ':siteAction',
+        loadComponent: () =>
+          import('./auth/online/online.component').then(
             (m) => m.OnlineComponent
           ),
       },
       {
         path: '',
         loadComponent: () =>
-          import('./auth-components/online/online.component').then(
+          import('./auth/online/online.component').then(
             (m) => m.OnlineComponent
           ),
       },
@@ -47,17 +64,15 @@ export const appRoutes: Routes = [
     path: 'verify-email',
     canActivate: [redirectUnauthorizedToOnline],
     loadComponent: () =>
-      import('./auth-components/verify/verify.component').then(
-        (m) => m.VerifyComponent
-      ),
+      import('./auth/verify/verify.component').then((m) => m.VerifyComponent),
   },
   {
     title: 'Forgot password',
     path: 'forgot-password',
     loadComponent: () =>
-      import(
-        './auth-components/forgot-password/forgot-password.component'
-      ).then((m) => m.ForgotPasswordComponent),
+      import('./auth/forgot-password/forgot-password.component').then(
+        (m) => m.ForgotPasswordComponent
+      ),
   },
   {
     title: 'Home - Memo-licious',
