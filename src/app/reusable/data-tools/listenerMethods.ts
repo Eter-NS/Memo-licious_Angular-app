46e5907cb089296e7ff7408bf1fdf94ac5233b50
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-02-29 18:21:59 +0100

    Created generic func type and darkModeListener function

--- src/app/reusable/data-tools/listenerMethods.ts
+++ src/app/reusable/data-tools/listenerMethods.ts
@@ -1,14 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function throttle<T extends (...args: any[]) => void>(
+type GenericFunction = (...args: any[]) => void;
+
+export type DarkModeSubscription = {
+  unsubscribe: () => void;
+};
+
+export function throttle<T extends GenericFunction>(
   func: T,
   limit: number = 1000
 ): (...args: Parameters<T>) => void {
   let inThrottle: boolean;
-  return function (...args: Parameters<T>): void {
+
+  return (...args: Parameters<T>): void => {
     if (!inThrottle) {
       func(...args);
       inThrottle = true;
       setTimeout(() => (inThrottle = false), limit);
     }
   };
+}
+
+export function darkModeListener<T extends GenericFunction>(
+  callback: T
+): DarkModeSubscription {
+  const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+
+  // First value just after subscribing
+  callback(matchMedia.matches);
+
+  // Further changes
+  matchMedia.addEventListener('change', callback);
+
+  const stopListening = () => {
+    window.removeEventListener('change', callback);
+  };
+
+  return { unsubscribe: stopListening };
 }
