08da80b1d6d5a2230451b65144265cf172b1375b
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-03-16 17:36:18 +0100

    Updated type checking in local-storage service

--- src/app/reusable/localStorage/local-storage.service.ts
+++ src/app/reusable/localStorage/local-storage.service.ts
@@ -11,18 +11,21 @@ export class LocalStorageService {
     return stringifiedData ? (JSON.parse(stringifiedData) as T) : null;
   }
 
-  checkElementInStorage<T>(id: string, key?: string): boolean {
+  checkElementInStorage<T extends object, K = keyof T>(
+    id: string,
+    key?: K
+  ): boolean {
     const data = this.loadFromStorage<T>(id);
     if (!data) {
       return false;
-    } else if (key) {
+    } else if (typeof data === 'object' && typeof key === 'string') {
       return hasNestedKey(data, key);
     } else {
       return true;
     }
   }
 
-  saveToStorage<T>(id: string, data: T): void {
+  saveToStorage(id: string, data: unknown): void {
     if (typeof data === 'string') {
       localStorage.setItem(id, data);
       return;
