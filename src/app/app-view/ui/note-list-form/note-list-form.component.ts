6c216342a8ad76b86d36425f4af2ecef54fb44a9
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-03-16 17:31:34 +0100

    Added note length validation with error messages, and updated imports

--- src/app/app-view/ui/note-list-form/note-list-form.component.ts
+++ src/app/app-view/ui/note-list-form/note-list-form.component.ts
@@ -20,16 +20,26 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { NoteModel } from 'src/app/auth/services/Models/UserDataModels';
+import { NoteModel } from 'src/app/auth/services/Models/UserDataModels.interface';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { MatInputModule } from '@angular/material/input';
-import { FormCommonFeaturesService } from 'src/app/auth/services/form-common-features/form-common-features.service';
+import { FormCommonFeaturesService } from 'src/app/reusable/data-access/form-common-features/form-common-features.service';
 import { AsyncPipe, NgTemplateOutlet } from '@angular/common';
 import { AdaptiveButtonComponent } from '../../../ui/adaptive-button/adaptive-button.component';
+import { BehaviorSubject } from 'rxjs';
 
 export type NewNoteGroupForm = ReturnType<
   typeof NoteListFormComponent.prototype.newNoteGroupForm.getRawValue
 >;
+
+export interface EditNoteI {
+  note: NoteModel;
+  event: MatChipEditedEvent;
+}
+
+export interface ValidationErrorI {
+  cause: string;
+}
 
 @Component({
   selector: 'app-note-list-form',
@@ -61,10 +71,7 @@ export class NoteListFormComponent {
   }
 
   @Output() createNote = new EventEmitter<MatChipInputEvent>();
-  @Output() editNote = new EventEmitter<{
-    note: NoteModel;
-    event: MatChipEditedEvent;
-  }>();
+  @Output() editNote = new EventEmitter<EditNoteI>();
   @Output() removeNote = new EventEmitter<NoteModel>();
   @Output() data = new EventEmitter<NewNoteGroupForm>();
 
@@ -75,10 +82,21 @@ export class NoteListFormComponent {
       validators: [Validators.required, Validators.maxLength(32)],
     }),
   });
-
   timeoutId!: unknown;
-
   separatorCodes = [ENTER, COMMA] as const;
+
+  #errorMessage = new BehaviorSubject<ValidationErrorI | null | undefined>(
+    undefined
+  );
+
+  get errorMessage$() {
+    return this.#errorMessage.asObservable();
+  }
+
+  #lengthValidationConfig = {
+    min: 0,
+    max: 35,
+  };
 
   getError = (element: string, validation: string) =>
     this.#formCommonFeaturesService.getError(
@@ -97,11 +115,64 @@ export class NoteListFormComponent {
     clearTimeout(this.timeoutId as number);
   }
 
+  /** For creating and modifying notes only */
+  handleNoteEvent(e: MatChipInputEvent | EditNoteI) {
+    if (!('event' in e) && !('value' in e)) {
+      console.error('The parameter is neither MatChipInputEvent nor EditNoteI');
+      return;
+    }
+
+    const isModificationEvent = 'event' in e;
+    const result = isModificationEvent
+      ? this._stringLengthValidator(e.event.value, this.#lengthValidationConfig)
+      : this._stringLengthValidator(e.value, this.#lengthValidationConfig);
+
+    if (!result) {
+      this.#errorMessage.next({
+        cause: `The note can't be longer than ${
+          this.#lengthValidationConfig.max
+        } characters`,
+      });
+    } else {
+      isModificationEvent ? this.editNote.emit(e) : this.createNote.emit(e);
+      this.#errorMessage.next(null);
+    }
+  }
+
   submitForm() {
     if (this.newNoteGroupForm.controls.groupName.errors) return;
 
     this.data.emit(this.newNoteGroupForm.getRawValue());
 
     this.newNoteGroupForm.reset();
+  }
+
+  private _stringLengthValidator(
+    value: string,
+    config: { min?: number; max?: number }
+  ) {
+    const throwNoOption = () => {
+      throw new Error('No configuration detected!');
+    };
+
+    if (!config) {
+      throwNoOption();
+    }
+
+    if (!Object.keys(config).length) {
+      throwNoOption();
+    }
+
+    const valueLength = value.length;
+    let result = false;
+
+    if (typeof config.min === 'number' && typeof config.max === 'number') {
+      result = config.min <= valueLength && valueLength <= config.max;
+    } else if (typeof config.min === 'number') {
+      result = config.min <= valueLength;
+    } else if (typeof config.max === 'number') {
+      result = valueLength <= config.max;
+    }
+    return result;
   }
 }
