860bd859f4dbcf69608dc5881ae4969ce168e183
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-05-03 17:07:48 +0200

    Moved @angular/fire methods to controller services.

---
+++ src/app/reusable/data-access/firebase-auth/firebase-auth-controller.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  createUserWithEmailAndPassword,
+  getRedirectResult,
+  reauthenticateWithCredential,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  updateEmail,
+  updatePassword,
+  updateProfile,
+} from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseAuthControllerService {
+  createUserWithEmailAndPassword = createUserWithEmailAndPassword;
+  signInWithEmailAndPassword = signInWithEmailAndPassword;
+  signInWithRedirect = signInWithRedirect;
+  signInWithPopup = signInWithPopup;
+  getRedirectResult = getRedirectResult;
+  signOut = signOut;
+  updateProfile = updateProfile;
+  updatePassword = updatePassword;
+  updateEmail = updateEmail;
+  reauthenticateWithCredential = reauthenticateWithCredential;
+}
