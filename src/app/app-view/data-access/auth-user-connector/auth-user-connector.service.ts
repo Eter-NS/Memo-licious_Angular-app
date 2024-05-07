de0fcd29c25e6567d3ddcf1a75d0078d76a2ed7f
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-05-07 13:24:31 +0200

    Created a user-profile service based on the modified auth-user-connector service.

--- src/app/app-view/data-access/auth-user-connector/auth-user-connector.service.ts
+++ src/app/app-view/data-access/auth-user-connector/auth-user-connector.service.ts
@@ -1,12 +1,10 @@
 import { Injectable, inject, signal } from '@angular/core';
 import {
-  EMPTY,
   Observable,
   catchError,
   combineLatest,
   map,
-  of,
-  switchMap,
+  tap,
   throwError,
 } from 'rxjs';
 import { LocalUserAccount } from 'src/app/auth/services/Models/LocalAuthModels.interface';
@@ -15,8 +13,11 @@ import { AuthLocalUserService } from 'src/app/auth/services/local-user/auth-loca
 import { AuthStateService } from 'src/app/auth/services/state/auth-state.service';
 import { ErrorHandlerService } from '../error-handler/error-handler.service';
 import { User } from '@angular/fire/auth';
-import { UserProfile } from '../../utils/models/user-profile.interface';
-import { UpdateUserInterface } from '../../utils/models/update-user.interface';
+import { UserProfileChangesI } from '../../utils/models/user-profile.interface';
+import { environment } from 'src/environments/environment.dev';
+import { RegisterCustomOptions } from 'src/app/auth/services/Models/OnlineAuthModels.interface';
+import { StorageService } from 'src/app/reusable/data-access/firebase-storage/storage.service';
+import { FILE_TO_BASE64_TOKEN } from 'src/app/reusable/utils/file-to-base64/file-to-base64.pipe.injector';
 
 export type UserType = 'online' | 'local' | null;
 
@@ -27,123 +28,216 @@ export class AuthUserConnectorService {
   #authLocalUserService = inject(AuthLocalUserService);
   #authAccountService = inject(AuthAccountService);
   #authStateService = inject(AuthStateService);
+  #storageService = inject(StorageService);
   #errorHandlerService = inject(ErrorHandlerService);
+  #fileToBase64Pipe = inject(FILE_TO_BASE64_TOKEN);
 
-  #previousUserType = signal<UserType>(null);
-  #activeUserType = signal<UserType>(null);
+  private _previousUserType = signal<UserType>(null);
+  private _activeUserType = signal<UserType>(null);
 
   get activeUserTypeSig() {
-    return this.#activeUserType.asReadonly();
+    return this._activeUserType.asReadonly();
   }
 
-  get activeUser$() {
-    return combineLatest([
-      this.#authLocalUserService.localUser$,
-      this.#authStateService.user$,
-    ]).pipe(
-      switchMap(([localUser, onlineUser]) => {
-        if (localUser) {
-          this._updateUserType('local');
-          return of(localUser);
-        }
+  activeUser$: Observable<LocalUserAccount | User | null> = combineLatest([
+    this.#authLocalUserService.localUser$,
+    this.#authStateService.user$,
+  ]).pipe(
+    map(([localUser, onlineUser]) => {
+      if (localUser && onlineUser) {
+        this.#authLocalUserService.logOut();
+        this.#authAccountService.signOutUser();
+        return null;
+      }
+      if (localUser) {
+        return localUser;
+      }
+      if (onlineUser) {
+        return onlineUser;
+      }
+      return null;
+    }),
+    tap((user) => {
+      if (!user && this.activeUserTypeSig() !== null) {
+        this._updateUserType(user);
+        return;
+      } else if (user) {
+        const userType = 'groups' in user ? 'local' : 'online';
 
-        if (onlineUser) {
-          this._updateUserType('online');
-          return of(onlineUser);
+        if (this.activeUserTypeSig() !== userType) {
+          this._updateUserType(userType);
         }
-
-        this._updateUserType(null);
-        if (this.#previousUserType()) {
-          // Ends the stream when user logs out
-          return EMPTY;
-        }
-
-        // Throws the error when no user was logged in ever before
-        throw new Error('No user logged in');
-      }),
-      catchError((err: Error) => {
+      }
+    }),
+    catchError((err) => {
+      if (!environment.production) {
         console.error(err);
-        this.#errorHandlerService.onError(err.message);
-        return throwError(() => err);
-      })
-    );
-  }
+      }
+      this.#errorHandlerService.onError('message' in err ? err.message : err);
+      return throwError(() => err);
+    })
+  );
 
-  get userProfile$(): Observable<UserProfile> {
-    return this.activeUser$.pipe(
-      map((user) => {
-        if (this.activeUserTypeSig() === 'local') {
-          const {
-            auth,
-            profileColor,
-            profilePictureUrl: photoUrl,
-          } = user as LocalUserAccount;
+  updateUser(changes: UserProfileChangesI): Promise<boolean> {
+    const userType = this.activeUserTypeSig();
 
-          return {
-            name: auth.name,
-            authOption: auth.authOption,
-            profileColor,
-            photoUrl,
-          } satisfies UserProfile;
-        }
-
-        const { photoURL: photoUrl, displayName, email } = user as User;
-
-        return {
-          name: displayName as string,
-          photoUrl,
-          authOption: 'password',
-          email: email as string,
-        } satisfies UserProfile;
-      })
-    );
-  }
-
-  private _updateUserType(value: UserType, period?: 'last' | 'current') {
-    switch (period) {
-      case 'current':
-        this.#activeUserType.set(value);
-        break;
-
-      case 'last':
-        this.#previousUserType.set(value);
-        break;
-
-      default:
-        this.#previousUserType.set(this.#activeUserType());
-        this.#activeUserType.set(value);
-        break;
+    if (!userType) {
+      return Promise.resolve(false);
     }
-  }
 
-  updateUser(changes: UpdateUserInterface) {
-    switch (this.activeUserTypeSig()) {
-      case 'local':
-        this.#authLocalUserService.modifyCurrentUser({ auth: changes.local });
-        break;
+    const handleMethods = {
+      online: () => this._handleOnlineUserUpdate(changes),
+      local: () => this._handleLocalUserUpdate(changes),
+    };
 
-      case 'online':
-        this.#authAccountService.changeUserProfileData(changes.online);
-        break;
-    }
+    return handleMethods[userType]();
   }
 
   logOutUser() {
-    if (!this.activeUserTypeSig()) {
+    const activeUser = this.activeUserTypeSig();
+    if (!activeUser) {
       return;
     }
 
-    switch (this.activeUserTypeSig()) {
-      case 'local':
-        this.#authLocalUserService.logOut();
-        break;
-      case 'online':
-        this.#authAccountService.signOutUser();
-        break;
-      default:
-        throw new Error('Unknown value of activeUserTypeSig');
-    }
+    const logoutDictionary = {
+      online: () => this.#authAccountService.signOutUser(),
+      local: () => this.#authLocalUserService.logOut(),
+    };
+
+    logoutDictionary[activeUser]();
 
     this._updateUserType(null);
+  }
+
+  private _updateUserType(newState: UserType) {
+    this._previousUserType.set(this.activeUserTypeSig());
+    this._activeUserType.set(newState);
+  }
+
+  private async _handleLocalUserUpdate(
+    changes: UserProfileChangesI
+  ): Promise<boolean> {
+    const { name, authOption, passphrase, photoBlob, profileColor } = changes;
+
+    if (photoBlob) {
+      const profilePictureUrl = await this.#fileToBase64Pipe.transform(
+        photoBlob.blob
+      );
+
+      this.#authLocalUserService.modifyCurrentUser({ profilePictureUrl });
+    }
+
+    if (!passphrase) {
+      return false;
+    }
+
+    return this.#authLocalUserService.modifyCurrentUser({
+      auth: { name, authOption, value: passphrase },
+      profileColor,
+    });
+  }
+
+  private async _handleOnlineUserUpdate(
+    changes: UserProfileChangesI
+  ): Promise<boolean> {
+    const {
+      photoUrl,
+      photoBlob,
+      name,
+      oldPassphrase,
+      passphrase,
+      oldEmail,
+      email,
+    } = changes;
+
+    try {
+      let photoURL = photoUrl;
+
+      if (photoBlob) {
+        const uid = this.#authStateService.sessionSig()?.uid;
+
+        if (!uid) {
+          return false;
+        }
+
+        const uploadResult = await this.#storageService.uploadFile(
+          `/users/${uid}/profile_pic.${photoBlob.extension}`,
+          photoBlob.blob
+        );
+
+        photoURL = await this.#storageService.getFileUrl(
+          uploadResult.ref.fullPath
+        );
+      }
+
+      const mainPayload: RegisterCustomOptions = {
+        displayName: name,
+        photoURL: photoURL || undefined,
+      };
+
+      await this.#authAccountService.changeUserProfileData(mainPayload);
+
+      if (!oldPassphrase) {
+        return true;
+      }
+
+      await this._handleOptionalPasswordUpdate(oldPassphrase, passphrase);
+      await this._handleOptionalEmailUpdate(
+        oldPassphrase,
+        passphrase,
+        oldEmail,
+        email
+      );
+
+      return true;
+    } catch (err) {
+      console.error('Updating online user ', err);
+      return false;
+    }
+  }
+
+  private async _handleOptionalPasswordUpdate(
+    oldPassphrase?: string,
+    passphrase?: string
+  ) {
+    if (oldPassphrase && passphrase) {
+      return await this.#authAccountService.updatePassword(
+        oldPassphrase,
+        passphrase
+      );
+    }
+
+    throw new Error('No existing password nor new password has been provided.');
+  }
+
+  private async _handleOptionalEmailUpdate(
+    existingPassphrase?: string,
+    newPassphrase?: string,
+    oldEmail?: string,
+    email?: string
+  ) {
+    if (!existingPassphrase) {
+      throw new Error('No existing password has been provided.');
+    }
+
+    const wasPasswordUpdated = !!existingPassphrase && !!newPassphrase;
+
+    if (oldEmail && email) {
+      const result = await this.#authAccountService.updateEmail(
+        wasPasswordUpdated ? newPassphrase : existingPassphrase,
+        oldEmail,
+        email
+      );
+
+      if (result.errors) {
+        if (result.errors.alreadyInUseError) {
+          throw new Error('Email already in use.');
+        }
+      }
+
+      return result;
+    }
+
+    throw new Error('No existing email nor new email has been provided.');
   }
 }
---
+++ src/app/app-view/data-access/user-profile/user-profile.service.ts
@@ -0,0 +1,93 @@
+import { Injectable, inject } from '@angular/core';
+import { AuthUserConnectorService } from '../auth-user-connector/auth-user-connector.service';
+import {
+  UserProfile,
+  UserProfileChangesI,
+} from '../../utils/models/user-profile.interface';
+import {
+  Observable,
+  Subject,
+  catchError,
+  filter,
+  from,
+  map,
+  of,
+  shareReplay,
+  switchMap,
+} from 'rxjs';
+import { User } from '@angular/fire/auth';
+import { LocalUserAccount } from 'src/app/auth/services/Models/LocalAuthModels.interface';
+
+export interface UserProfileUpdateResultI {
+  state: 'pending' | 'success' | 'failure' | 'idle';
+  cause?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserProfileService {
+  #authUserConnectorService = inject(AuthUserConnectorService);
+
+  get userProfile$(): Observable<UserProfile> {
+    return this.#authUserConnectorService.activeUser$.pipe(
+      filter((user): user is LocalUserAccount | User => !!user),
+      map((user): UserProfile => this._createUserProfile(user))
+    );
+  }
+
+  #userProfileUpdateSubject = new Subject<UserProfileChangesI>();
+  get userProfileUpdateAction$() {
+    return this.#userProfileUpdateSubject.asObservable().pipe(
+      switchMap((value) =>
+        from(this.#authUserConnectorService.updateUser(value))
+      ),
+      shareReplay({ refCount: false, bufferSize: 1, windowTime: 1000 })
+    );
+  }
+
+  get userProfileUpdateResult$(): Observable<UserProfileUpdateResultI> {
+    return this.userProfileUpdateAction$.pipe(
+      map(
+        (result): UserProfileUpdateResultI => ({
+          state: result ? 'success' : 'failure',
+        })
+      ),
+      catchError((err) => {
+        return of<UserProfileUpdateResultI>({
+          state: 'failure',
+          cause: 'message' in err ? err.message : err,
+        });
+      })
+    );
+  }
+
+  uploadProfileChanges(changes: UserProfileChangesI) {
+    this.#userProfileUpdateSubject.next(changes);
+  }
+
+  private _createUserProfile(user: LocalUserAccount | User): UserProfile {
+    if (this.#authUserConnectorService.activeUserTypeSig() === 'local') {
+      const {
+        auth: { name, authOption },
+        profilePictureUrl: photoUrl,
+        profileColor,
+      } = user as LocalUserAccount;
+
+      return {
+        name,
+        photoUrl,
+        authOption,
+        profileColor,
+      };
+    }
+
+    const { photoURL: photoUrl, displayName } = user as User;
+
+    return {
+      name: displayName as string,
+      photoUrl,
+      authOption: 'password',
+    };
+  }
+}
