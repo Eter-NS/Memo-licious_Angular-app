7c6add4f14ee229efec6b4fec14e01aaee9d65e2
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-05-07 13:53:43 +0200

    Removed unused interfaces for updating user credentials.

--- src/app/app-view/utils/models/update-user.interface.ts
+++
@@ -1,7 +0,0 @@
-import { LocalUserAuth } from 'src/app/auth/services/Models/LocalAuthModels.interface';
-import { RegisterCustomOptions } from 'src/app/auth/services/Models/OnlineAuthModels.interface';
-
-export interface UpdateUserInterface {
-  local: LocalUserAuth;
-  online: RegisterCustomOptions;
-}
