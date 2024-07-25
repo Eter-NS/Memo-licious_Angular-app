a14a77dcb02a305a8941dd3eadd565b328f3fc2d
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-07-25 03:37:35 +0200

    Add tests to note-rest service

--- src/app/app-view/data-access/note-REST/note-rest.service.spec.ts
+++ src/app/app-view/data-access/note-REST/note-rest.service.spec.ts
@@ -1,16 +1,339 @@
-import { TestBed } from '@angular/core/testing';
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { NoteRestService } from './note-rest.service';
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { NoteModel } from 'src/app/auth/utils/Models/UserDataModels.interface';
+import { Observable } from 'rxjs';
+import { Provider } from '@angular/core';
+import { TIMESTAMP_TOKEN } from 'src/app/reusable/data-access/timestamp/timestamp.token';
+import { localUTCTimestamp } from 'src/app/reusable/utils/data-tools/objectTools';
 
-describe('NotesRestService', () => {
+describe('NoteRestService', () => {
+  const liveAnnouncerMock = jasmine.createSpyObj<LiveAnnouncer>(['announce']);
+
+  const exampleNoteModel: NoteModel = {
+    createdAt: Date.now(),
+    id: 'xxxx',
+    value: 'Hello World!',
+  };
+
   let service: NoteRestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        NoteRestService,
+        { provide: LiveAnnouncer, useValue: liveAnnouncerMock },
+        { provide: TIMESTAMP_TOKEN, useFactory: () => localUTCTimestamp },
+      ] satisfies Provider[],
+    });
     service = TestBed.inject(NoteRestService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe(`notesBuffer$`, () => {
+    it(`should return an Observable that emits empty notesBuffer (right after creating the service)`, fakeAsync(() => {
+      expect(service.notesBuffer$ instanceof Observable).toBeTruthy();
+
+      const subscription = service.notesBuffer$.subscribe((buffer) => {
+        expect(buffer).toEqual([]);
+      });
+
+      tick();
+
+      subscription.unsubscribe();
+    }));
+  });
+
+  describe(`ngOnDestroy()`, () => {
+    it(`should reset notesBuffer$ to empty array.`, () => {
+      // Arrange
+      let noteModels: NoteModel[] = [];
+      service.fillNotesBuffer([exampleNoteModel]);
+
+      // Act
+      const subscription = service.notesBuffer$.subscribe((value) => {
+        noteModels = value;
+      });
+
+      // Assert
+      expect(noteModels.length).toBe(1);
+
+      // Act
+      service.ngOnDestroy();
+
+      subscription.unsubscribe();
+
+      // Assert
+      expect(noteModels.length).toBe(0);
+    });
+
+    it(`should complete notesBuffer$.`, () => {
+      // Arrange
+      let result = false;
+
+      // Act
+      const subscription = service.notesBuffer$.subscribe({
+        complete: () => (result = true),
+      });
+
+      service.ngOnDestroy();
+
+      subscription.unsubscribe();
+
+      // Assert
+      expect(result).toBe(true);
+    });
+  });
+
+  describe(`fillNotesBuffer()`, () => {
+    it(`should pass an array of noteModels to notesBuffer$.`, () => {
+      // Arrange
+      const payload: NoteModel[] = [exampleNoteModel];
+      let noteModels: NoteModel[] = [];
+
+      // Act
+
+      const subscription = service.notesBuffer$.subscribe((value) => {
+        noteModels = value;
+      });
+
+      // Assert
+      expect(noteModels.length).toBe(0);
+
+      // Act
+      service.fillNotesBuffer(payload);
+
+      subscription.unsubscribe();
+
+      // Assert
+      expect(noteModels.length).toEqual(1);
+    });
+  });
+
+  describe(`onCreateNote()`, () => {
+    it(`should stop executing the method if value is empty`, async () => {
+      const spy = spyOn(service as any, '_createNote');
+      const payload = {
+        value: '',
+        chipInput: { clear: () => {} },
+      } as MatChipInputEvent;
+
+      await service.onCreateNote(payload);
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it(`should clear chip input after successful note creation`, async () => {
+      const payload = {
+        value: 'example value',
+        chipInput: { clear: () => {} },
+      } as MatChipInputEvent;
+      spyOn(service as any, '_createNote').and.resolveTo(undefined);
+      const spy = spyOn(payload.chipInput, 'clear');
+
+      await service.onCreateNote(payload);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe(`onEditNote()`, () => {
+    it('should call onRemoveNote if note is empty', () => {
+      const spy = spyOn(service as any, '_editNote');
+      const payload = {
+        event: {
+          value: '',
+        },
+        note: {},
+      } as { note: NoteModel; event: MatChipEditedEvent };
+
+      service.onEditNote(payload);
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it(`should call noteService.editNote() if note is not empty`, () => {
+      const spy = spyOn(service as any, '_editNote');
+      const payload = {
+        event: {
+          value: 'some value',
+        },
+        note: {},
+      } as { note: NoteModel; event: MatChipEditedEvent };
+
+      service.onEditNote(payload);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe(`onRemoveNote()`, () => {
+    it(`should not announce note removal if note wasn't removed`, () => {
+      spyOn(service as any, '_deleteNote').and.returnValue(false);
+      const spy = liveAnnouncerMock.announce;
+      const payload = {
+        id: '123',
+        value: 'some value',
+      } as NoteModel;
+
+      service.onRemoveNote(payload);
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it(`should  announce note removal if note was removed`, () => {
+      spyOn(service as any, '_deleteNote').and.returnValue(true);
+      const spy = liveAnnouncerMock.announce;
+      const payload = {
+        id: '123',
+        value: 'some value',
+      } as NoteModel;
+
+      service.onRemoveNote(payload);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe(`_createNote()`, () => {
+    it(`should log the warning when an empty string comes as an argument.`, async () => {
+      // Arrange
+      const spy = spyOn(console, 'warn').and.stub();
+
+      // Act
+      await service['_createNote']('');
+
+      // Assert
+      expect(spy).toHaveBeenCalledWith('No value provided to new note');
+    });
+
+    it(`should call _notesBufferSubject.next() with the new note.`, async () => {
+      // Arrange
+      const spy = spyOn(
+        service['_notesBufferSubject'],
+        'next'
+      ).and.callThrough();
+
+      // Act
+      await service['_createNote']('some value');
+
+      // Assert
+      const notesModel = service['_notesBufferSubject'].value.findIndex(
+        (note) => note.value === 'some value'
+      );
+      expect(spy).toHaveBeenCalled();
+      expect(notesModel).toBeGreaterThan(-1);
+    });
+  });
+
+  describe(`_editNote()`, () => {
+    it(`should return false if a note hasn't changed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+
+      // Act
+      const result = service['_editNote']('xyz2', { value: 'new value' });
+
+      // Assert
+      expect(result).toBeFalse();
+    });
+
+    it(`should not call _notesBufferSubject.next() if a note hasn't changed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+      const spy = spyOn(
+        service['_notesBufferSubject'],
+        'next'
+      ).and.callThrough();
+
+      // Act
+      const result = service['_editNote']('xyz2', { value: 'new value' });
+
+      // Assert
+      expect(spy).not.toHaveBeenCalled();
+      expect(result).toBeFalse();
+    });
+
+    it(`should call _notesBufferSubject.next() and return true if a note has changed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+      const spy = spyOn(
+        service['_notesBufferSubject'],
+        'next'
+      ).and.callThrough();
+
+      // Act
+      const result = service['_editNote']('xyz', { value: 'new value' });
+
+      // Assert
+      const noteModel = service['_notesBufferSubject'].value.find(
+        ({ id }) => id === 'xyz'
+      );
+      expect(spy).toHaveBeenCalled();
+      expect(result).toBeTrue();
+      expect(noteModel?.value).toBe('new value');
+    });
+  });
+
+  describe(`_deleteNote()`, () => {
+    it(`should return false if a note hasn't been removed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+
+      // Act
+      const result = service['_deleteNote']('xyz2');
+
+      // Assert
+      expect(result).toBeFalse();
+    });
+
+    it(`should not call _notesBufferSubject.next() if a note hasn't been removed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+      const spy = spyOn(
+        service['_notesBufferSubject'],
+        'next'
+      ).and.callThrough();
+
+      // Act
+      const result = service['_deleteNote']('xyz2');
+
+      // Assert
+      expect(spy).not.toHaveBeenCalled();
+      expect(result).toBeFalse();
+    });
+
+    it(`should call _notesBufferSubject.next() and return true if a note has been removed.`, () => {
+      // Arrange
+      service.fillNotesBuffer([
+        { id: 'xyz', createdAt: Date.now(), value: 'current value' },
+      ]);
+      const spy = spyOn(
+        service['_notesBufferSubject'],
+        'next'
+      ).and.callThrough();
+
+      // Act
+      const result = service['_deleteNote']('xyz');
+
+      // Assert
+      expect(spy).toHaveBeenCalled();
+      expect(result).toBeTrue();
+    });
   });
 });
--- src/app/app-view/data-access/note-REST/note-rest.service.ts
+++ src/app/app-view/data-access/note-REST/note-rest.service.ts
@@ -1,19 +1,40 @@
-import { Injectable, inject } from '@angular/core';
-import { NotesService } from '../notes/notes.service';
+import { Injectable, OnDestroy, inject } from '@angular/core';
 import { MatChipInputEvent, MatChipEditedEvent } from '@angular/material/chips';
 import { NoteModel } from 'src/app/auth/utils/Models/UserDataModels.interface';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { randomId } from 'src/app/reusable/utils/data-tools/objectTools';
+import { BehaviorSubject } from 'rxjs';
+import { TIMESTAMP_TOKEN } from 'src/app/reusable/data-access/timestamp/timestamp.token';
 
 @Injectable()
-export class NoteRestService {
-  #notesService = inject(NotesService);
+export class NoteRestService implements OnDestroy {
   #announcer = inject(LiveAnnouncer);
+  #timestamp = inject(TIMESTAMP_TOKEN);
+
+  readonly ID_LENGTH = 27;
+
+  private _notesBufferSubject = new BehaviorSubject<NoteModel[]>([]);
+
+  get notesBuffer$() {
+    return this._notesBufferSubject.asObservable();
+  }
+
+  ngOnDestroy(): void {
+    this.fillNotesBuffer([]);
+    this._notesBufferSubject.complete();
+  }
+
+  fillNotesBuffer(notes: NoteModel[]) {
+    this._notesBufferSubject.next(notes);
+  }
 
   async onCreateNote(event: MatChipInputEvent) {
     const value = event.value.trim();
-    if (!value) return;
+    if (!value) {
+      return;
+    }
 
-    await this.#notesService.createNote(value);
+    await this._createNote(value);
 
     event.chipInput.clear();
   }
@@ -31,12 +52,66 @@ export class NoteRestService {
       value: trimmedValue,
     };
 
-    this.#notesService.editNote(note.id, payload);
+    this._editNote(note.id, payload);
   }
 
   onRemoveNote({ id, value }: NoteModel) {
-    if (!this.#notesService.deleteNote(id)) return;
+    if (!this._deleteNote(id)) {
+      return;
+    }
 
     this.#announcer.announce(`Removed ${value}`);
+  }
+
+  /* NoteModel methods */
+
+  private async _createNote(value: string) {
+    if (!value) {
+      console.warn('No value provided to new note');
+      return;
+    }
+
+    const createdAt = await this.#timestamp();
+    const note: NoteModel = {
+      createdAt,
+      id: randomId(this.ID_LENGTH),
+      value,
+    };
+
+    this._notesBufferSubject.next([...this._notesBufferSubject.value, note]);
+  }
+
+  private _editNote(noteId: string, changes: Partial<NoteModel>): boolean {
+    let isEdited = false;
+
+    const updatedNotes = this._notesBufferSubject.value.map((note) => {
+      if (note.id === noteId) {
+        isEdited = true;
+        return { ...note, ...changes };
+      }
+
+      return note;
+    });
+
+    if (isEdited) {
+      this._notesBufferSubject.next(updatedNotes);
+    }
+
+    return isEdited;
+  }
+
+  private _deleteNote(id: string): boolean {
+    const initialLength = this._notesBufferSubject.value.length;
+
+    const leftNotes = this._notesBufferSubject.value.filter(
+      (note) => note.id !== id
+    );
+    const isEqualLength = leftNotes.length !== initialLength;
+
+    if (isEqualLength) {
+      this._notesBufferSubject.next(leftNotes);
+    }
+
+    return isEqualLength;
   }
 }
