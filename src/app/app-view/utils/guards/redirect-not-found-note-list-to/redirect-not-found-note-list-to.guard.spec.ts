32fda2a5a939cb675e828063147a35237a318f17
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-08-19 22:17:08 +0200

    Add tests for redirectNotFoundNoteListTo guard

--- src/app/app-view/utils/guards/redirect-not-found-note-list-to/redirect-not-found-note-list-to.guard.spec.ts
+++ src/app/app-view/utils/guards/redirect-not-found-note-list-to/redirect-not-found-note-list-to.guard.spec.ts
@@ -1,17 +1,135 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 
 import { redirectNotFoundNoteListToGuard } from './redirect-not-found-note-list-to.guard';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { NoteGroupModel } from 'src/app/auth/utils/Models/UserDataModels.interface';
+import { Provider } from '@angular/core';
+import { NotesService } from 'src/app/app-view/data-access/notes/notes.service';
 
 describe('redirectNotFoundNoteListToGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => redirectNotFoundNoteListToGuard(...guardParameters));
+  // Mocks
+  const notesSubject = new BehaviorSubject<NoteGroupModel[]>([]);
+  const notesServiceMock = {
+    notes$: notesSubject.asObservable(),
+  };
+  const routerMock = jasmine.createSpyObj<Router>(['createUrlTree']);
+
+  const executeGuard = (denyFallback: string[]): CanActivateFn => {
+    return TestBed.runInInjectionContext(() =>
+      redirectNotFoundNoteListToGuard(denyFallback)
+    );
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: routerMock },
+        { provide: NotesService, useValue: notesServiceMock },
+      ] satisfies Provider[],
+    });
   });
 
-  it('should be created', () => {
+  it('should be created.', () => {
     expect(executeGuard).toBeTruthy();
   });
+
+  it(`should return true if the url ends with an existing group id.`, fakeAsync(() => {
+    // Arrange
+    notesSubject.next([
+      {
+        id: 'xxx',
+      } as NoteGroupModel,
+    ]);
+
+    // Act
+    TestBed.runInInjectionContext(() => {
+      let value: boolean | UrlTree | undefined;
+
+      const guard = executeGuard(['/notes']);
+
+      const subscription = (
+        guard(
+          {} as ActivatedRouteSnapshot,
+          { url: '/notes/xxx' } as RouterStateSnapshot
+        ) as Observable<true | UrlTree>
+      ).subscribe((state) => {
+        value = state;
+      });
+
+      tick();
+      subscription.unsubscribe();
+
+      // Assert
+      expect(value).toBe(true);
+    });
+  }));
+
+  it(`should return UrlTree if the url does NOT contain group id.`, fakeAsync(() => {
+    // Arrange
+    const spy = routerMock.createUrlTree.and.callFake(() => new UrlTree());
+    notesSubject.next([
+      {
+        id: 'xxx2',
+      } as NoteGroupModel,
+    ]);
+
+    // Act
+    TestBed.runInInjectionContext(() => {
+      let value: boolean | UrlTree | undefined;
+
+      const guard = executeGuard(['/notes']);
+
+      const subscription = (
+        guard(
+          {} as ActivatedRouteSnapshot,
+          { url: '/notes/xxx' } as RouterStateSnapshot
+        ) as Observable<true | UrlTree>
+      ).subscribe((state) => {
+        value = state;
+      });
+
+      tick();
+      subscription.unsubscribe();
+
+      // Assert
+      expect(spy).toHaveBeenCalled();
+      expect(value instanceof UrlTree).toBe(true);
+    });
+  }));
+
+  it(`should return UrlTree if the url does NOT contain group id.`, fakeAsync(() => {
+    // Arrange
+    const spy = routerMock.createUrlTree.and.callFake(() => new UrlTree());
+    notesSubject.error(new Error('Example error'));
+
+    // Act
+    TestBed.runInInjectionContext(() => {
+      let value: boolean | UrlTree | undefined;
+
+      const guard = executeGuard(['/notes']);
+
+      const subscription = (
+        guard(
+          {} as ActivatedRouteSnapshot,
+          { url: '/notes/xxx' } as RouterStateSnapshot
+        ) as Observable<true | UrlTree>
+      ).subscribe((state) => {
+        value = state;
+      });
+
+      tick();
+      subscription.unsubscribe();
+
+      // Assert
+      expect(spy).toHaveBeenCalled();
+      expect(value instanceof UrlTree).toBe(true);
+    });
+  }));
 });
