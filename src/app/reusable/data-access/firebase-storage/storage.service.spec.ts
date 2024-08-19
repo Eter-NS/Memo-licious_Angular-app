a38f52e90151ba67cf5a901dce9990c90624c695
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-08-19 22:38:57 +0200

    Add tests for firebase Storage service and rename _fireStorageController to #fireStorageController

--- src/app/reusable/data-access/firebase-storage/storage.service.spec.ts
+++ src/app/reusable/data-access/firebase-storage/storage.service.spec.ts
@@ -1,16 +1,73 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
+import { firebaseStorageControllerService } from './firebase-storage-controller.service.mock';
+import { Provider } from '@angular/core';
+import { FirebaseStorageControllerService } from './firebase-storage-controller.service';
+import { UploadResult } from '@angular/fire/storage';
 
 describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: FirebaseStorageControllerService,
+          useValue: firebaseStorageControllerService,
+        },
+      ] satisfies Provider[],
+    });
+
     service = TestBed.inject(StorageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe(`Methods`, () => {
+    describe(`getBlob()`, () => {
+      it(`should return a promise with the blob`, () => {
+        // Arrange
+        firebaseStorageControllerService.getBlob.and.resolveTo(new Blob());
+
+        // Act
+        const result = service.getBlob('/xyz');
+
+        // Assert
+        expect(result instanceof Promise).toBeTruthy();
+      });
+    });
+
+    describe(`getFileUrl()`, () => {
+      it(`should return a promise with url string.`, () => {
+        // Arrange
+        firebaseStorageControllerService.getDownloadURL.and.resolveTo(
+          'https://example.com/logo.svg'
+        );
+
+        // Act
+        const result = service.getFileUrl('/xyz');
+
+        // Assert
+        expect(result instanceof Promise).toBeTruthy();
+      });
+    });
+
+    describe(`uploadFile()`, () => {
+      it(`should return a promise of UploadResult.`, () => {
+        // Arrange
+        firebaseStorageControllerService.uploadBytes.and.resolveTo(
+          {} as UploadResult
+        );
+
+        // Act
+        const result = service.uploadFile('', new Blob());
+
+        // Assert
+        expect(result instanceof Promise).toBeTruthy();
+      });
+    });
   });
 });
--- src/app/reusable/data-access/firebase-storage/storage.service.ts
+++ src/app/reusable/data-access/firebase-storage/storage.service.ts
@@ -1,31 +1,30 @@
 import { Injectable, inject } from '@angular/core';
-import { Storage } from '@angular/fire/storage';
 import { FirebaseStorageControllerService } from './firebase-storage-controller.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
-  storage = inject(Storage);
-  private readonly _fireStorageController = inject(
-    FirebaseStorageControllerService
-  );
+  #fireStorageController = inject(FirebaseStorageControllerService);
 
   getBlob(path: string) {
-    return this._fireStorageController.getBlob(
-      this._fireStorageController.ref(this.storage, path)
+    return this.#fireStorageController.getBlob(
+      this.#fireStorageController.ref(this.#fireStorageController.storage, path)
     );
   }
 
   getFileUrl(path: string) {
-    return this._fireStorageController.getDownloadURL(
-      this._fireStorageController.ref(this.storage, path)
+    return this.#fireStorageController.getDownloadURL(
+      this.#fireStorageController.ref(this.#fireStorageController.storage, path)
     );
   }
 
   uploadFile(path: string, file: Blob) {
-    return this._fireStorageController.uploadBytes(
-      this._fireStorageController.ref(this.storage, path),
+    return this.#fireStorageController.uploadBytes(
+      this.#fireStorageController.ref(
+        this.#fireStorageController.storage,
+        path
+      ),
       file
     );
   }
