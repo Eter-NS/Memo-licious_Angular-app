1fac6cf52bf340c9dbcd5775c1e08700c4550063
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-08-20 03:43:08 +0200

    Rewrite FileToBase64 pipe and add tests

--- src/app/reusable/utils/file-to-base64/file-to-base64.pipe.spec.ts
+++ src/app/reusable/utils/file-to-base64/file-to-base64.pipe.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FileToBase64Pipe } from './file-to-base64.pipe';
 
 describe('FileToBase64Pipe', () => {
@@ -11,9 +12,33 @@ describe('FileToBase64Pipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should return a string', async () => {
+  it('should return a Base64 string.', async () => {
     const result = await pipe.transform(new File([], 'test.txt'));
 
     expect(typeof result).toBe('string');
+  });
+
+  it('should throw an error during invalid transformation.', async () => {
+    // Arrange
+    spyOn(console, 'error').and.stub();
+
+    window.FileReader = class mockedFileReader extends FileReader {
+      override readAsDataURL(value: File | Blob) {
+        value;
+        this.onerror?.({
+          target: {
+            error: {
+              message: 'example error from onerror',
+            },
+          },
+        } as ProgressEvent<FileReader>);
+      }
+    };
+
+    // Act
+    const result = await pipe.transform({} as File);
+
+    // Assert
+    expect(result).toEqual(null);
   });
 });
--- src/app/reusable/utils/file-to-base64/file-to-base64.pipe.ts
+++ src/app/reusable/utils/file-to-base64/file-to-base64.pipe.ts
@@ -1,17 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment.dev';
 
 @Pipe({
   name: 'fileToBase64',
   standalone: true,
 })
 export class FileToBase64Pipe implements PipeTransform {
-  transform(value: File | Blob): Promise<string> {
+  transform(value: File | Blob): Promise<string | null> {
     const reader = new FileReader();
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
+      // Set callbacks
       reader.onload = (event) => resolve(event.target?.result as string);
-      reader.onerror = reject;
+      reader.onerror = (event) => {
+        if (!environment.production) {
+          console.error(event.target?.error);
+        }
+        resolve(null);
+      };
 
+      // Initialize reader
       reader.readAsDataURL(value);
     });
   }
