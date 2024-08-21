150e003616d46e985c5837e79158c9f574bffc43
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-08-21 21:28:33 +0200

    Improve readability of isAuthError helper function

--- src/app/reusable/utils/Models/isAuthError.ts
+++ src/app/reusable/utils/Models/isAuthError.ts
@@ -1,12 +1,7 @@
 import { FirebaseAuthError } from 'src/app/auth/utils/Models/OnlineAuthModels.interface';
 
 export function isAuthError(error: unknown): error is FirebaseAuthError {
-  if (
-    error &&
-    typeof error === 'object' &&
-    'message' in error &&
-    'code' in error
-  ) {
-    return true;
-  } else return false;
+  return Boolean(
+    error && typeof error === 'object' && 'code' in error && 'message' in error
+  );
 }
