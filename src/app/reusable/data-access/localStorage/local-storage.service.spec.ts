a6ba958044177b2feeb5a734f4c9224eac57f378
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-08-21 21:31:19 +0200

    Update and improve test suite for LocalStorage service

--- src/app/reusable/data-access/localStorage/local-storage.service.spec.ts
+++ src/app/reusable/data-access/localStorage/local-storage.service.spec.ts
@@ -10,72 +10,104 @@ describe('LocalStorageService', () => {
     service = TestBed.inject(LocalStorageService);
   });
 
+  afterEach(() => {
+    localStorage.clear();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   describe('saveToStorage()', () => {
     it('should save data to storage', () => {
-      service.saveToStorage('test', { test: 'test' });
-      expect(localStorage.getItem('test')).toEqual(
-        JSON.stringify({ test: 'test' })
-      );
+      const token = 'test';
+      const payload = { testKey: 'test-value' };
+
+      service.saveToStorage(token, payload);
+
+      expect(localStorage.getItem(token)).toEqual(JSON.stringify(payload));
     });
 
     it('should override data in storage', () => {
-      service.saveToStorage('test', { test: 'test' });
-      service.saveToStorage('test', { test: '' });
-      expect(localStorage.getItem('test')).toEqual(
-        JSON.stringify({ test: '' })
-      );
+      const token = 'test';
+      const payload = { testKey: 'test-value' };
+      service.saveToStorage(token, payload);
+
+      payload.testKey = '';
+
+      service.saveToStorage(token, payload);
+
+      expect(localStorage.getItem(token)).toEqual(JSON.stringify(payload));
     });
 
-    afterEach(() => {
-      localStorage.clear();
+    it(`should write the data directly if the type of it is string.`, () => {
+      // Arrange
+      const token = 'test';
+      const payload = 'string-value';
+
+      // Act
+      service.saveToStorage(token, payload);
+
+      // Assert
+      expect(localStorage.getItem(token)).toEqual(payload);
     });
   });
 
   describe('loadFromStorage()', () => {
     it('should return null if there is no data in storage', () => {
       const data = service.loadFromStorage('test');
+
       expect(data).toBeNull();
     });
 
     it('should return element if there is such data in storage, with a correct types', () => {
-      localStorage.setItem('test', JSON.stringify({ test: 'test' }));
-      const data = service.loadFromStorage<{ test: string }>('test');
-      expect(data).toEqual({ test: 'test' });
+      const token = 'test';
+      const storedData = { testKey: 'test-value' };
+
+      localStorage.setItem(token, JSON.stringify(storedData));
+
+      const data = service.loadFromStorage<typeof storedData>(token);
+
+      expect(data).toEqual(storedData);
     });
   });
 
   describe('checkElementInStorage()', () => {
     it('should return true if there is such data in storage (id)', () => {
-      localStorage.setItem('test', JSON.stringify({ testExample: 'test' }));
-      const data = service.checkElementInStorage('test');
+      const token = 'test';
+      localStorage.setItem(token, JSON.stringify({ testExample: 'test' }));
+
+      const data = service.checkElementInStorage(token);
+
       expect(data).toBeTrue();
     });
 
     it('should return true if there is such data in storage (id + object data structure)', () => {
-      localStorage.setItem('test', JSON.stringify({ testExample: 'test' }));
-      const data = service.checkElementInStorage('test', 'testExample');
+      const token = 'test';
+      localStorage.setItem(token, JSON.stringify({ testExample: 'test' }));
+
+      const data = service.checkElementInStorage(token, 'testExample');
+
       expect(data).toBeTrue();
     });
 
     it('should return false if there is Not such data in storage', () => {
-      const data = service.checkElementInStorage('test', 'testExample');
-      expect(data).toBeFalse();
-    });
+      const token = 'test';
 
-    afterEach(() => {
-      localStorage.clear();
+      const data = service.checkElementInStorage(token, 'testExample');
+
+      expect(data).toBeFalse();
     });
   });
 
   describe('removeFromStorage', () => {
     it('should remove data from storage', () => {
-      localStorage.setItem('test', JSON.stringify({ testExample: 'test' }));
-      service.removeFromStorage('test');
-      expect(localStorage.getItem('test')).toBeNull();
+      const token = 'test';
+      localStorage.setItem(token, JSON.stringify({ testExample: 'test' }));
+
+      service.removeFromStorage(token);
+
+      expect(localStorage.getItem(token)).toBeNull();
     });
   });
 });
