16335b02f2e45f20cd1dc62c96d40ab67e2f6346
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-09-30 18:55:06 +0200

    Add karma-viewport to dev dependencies
    Additional changes:
    - Update testing config,
    - Fix not consistent firebase emulator export.

---
+++ karma.conf.ts
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+/// <reference types="karma-viewport" />
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-viewport'),
+    ],
+    client: {
+      jasmine: {
+        random: false,
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/memo-licious'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 1000 * 60 * 60,
+    // Viewport configuration
+    viewport: {
+      breakpoints: [
+        {
+          name: 'mobile',
+          size: {
+            width: 320,
+            height: 480,
+          },
+        },
+        {
+          name: 'tablet',
+          size: {
+            width: 768,
+            height: 1024,
+          },
+        },
+        {
+          name: 'screen',
+          size: {
+            width: 1440,
+            height: 900,
+          },
+        },
+      ],
+    },
+  });
+};
