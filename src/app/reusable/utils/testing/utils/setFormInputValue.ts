9a740ff039b28eab39ffe41b4425db51b3df73b8
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2024-11-11 00:16:24 +0100

    Fix issue with tests failing on unfocused form input updates

--- src/app/reusable/utils/testing/utils/setFormInputValue.ts
+++ src/app/reusable/utils/testing/utils/setFormInputValue.ts
@@ -2,5 +2,5 @@ export function setFormInputValue(el: HTMLInputElement, value: string) {
   el.focus();
   el.value = value;
   el.dispatchEvent(new Event('input'));
-  el.blur();
+  el.dispatchEvent(new Event('blur'));
 }
